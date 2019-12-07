80fbb2e4a6bc72b46587ab9470ef03aaec016ebe
zhumingye <zhumingye@163.com>
2019-12-07 16:40:25 +0800

    Change-Id: Id3166ab242fea8af103c9c7cfe840dd049f843fc Signed-off-by: zhumingye <zhumingye@163.com>

--- src/main/java/com/DemoAll/Utils/IOToFileUtils.java
+++ src/main/java/com/DemoAll/Utils/IOToFileUtils.java
@@ -328,6 +328,7 @@ public class IOToFileUtils {
            // int line = 1;
             //一次读入一行，直到读入null为文件结束
             while((tempString = reader.readLine())!=null){
+            	System.out.println(tempString);
                 //显示行号
                // System.out.println("line "+line+":"+tempString);
                // line++;
